829d354ceea4cf7d957e9ffff5f1b09be3964173
Akashkumar S <s.akashkumar004@gmail.com>
2025-05-07 22:36:13 +0530

    Add files via upload

--- NPO-website/npo-server/server.js
+++ NPO-website/npo-server/server.js
@@ -4,7 +4,11 @@ const app=express();
 const cors=require('cors');
 const mongoose=require('mongoose');
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://npo-website.vercel.app/',
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
