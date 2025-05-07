5f27308919f1fcc4ccb0739e4cd82a70bdf1c4e5
Akashkumar S <s.akashkumar004@gmail.com>
2025-05-07 22:30:14 +0530

    Add files via upload

--- NPO-website/npo-server/server.js
+++ NPO-website/npo-server/server.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 const express=require('express')
 const app=express();
 const cors=require('cors');
 const mongoose=require('mongoose');
 app.use(express.json());
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 const authroute=require('./routes/userroute');
 const serviceroute=require('./routes/serviceroute');
@@ -16,6 +18,6 @@ app.use('/query',queryroute);
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5001, () => console.log('Server running on port 5001'));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
